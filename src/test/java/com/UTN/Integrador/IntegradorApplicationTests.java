9ef811fedebaf7257f0cc8732d1152ab67d52f1e
Julian Schaab <jupobatero1@gmail.com>
2022-06-13 21:22:35 -0300

    Commit inicial

---
+++ src/test/java/com/UTN/Integrador/IntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UTN.Integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
